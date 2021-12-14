6d3082c4a43e69c2e580568dcb4aee253121c027
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-12-14 22:01:22 +0530

    Remove theme

--- web-app/theme/Minia_React_v1.0.0/Minia-Typescript/Starterkit-Admin-Hooks-Typescript/src/components/NonAuthLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-
-const NonAuthLayout = ({ children }: any) => {
-  return <React.Fragment>{children}</React.Fragment>;
-};
-
-export default withRouter(NonAuthLayout);
