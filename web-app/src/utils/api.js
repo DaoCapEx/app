fae9405d51bd1c5290e3fa1dc50928137443f18a
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-12-18 20:58:20 +0530

    add api

--- web-app/src/utils/api.js
+++ web-app/src/utils/api.js
@@ -1,21 +1,28 @@
 import axios from 'axios';
+import Cookie from './cookie'
 
 export default class API {
 
-    static backendPath = process.env.BAKEND_HOST + ":" + process.env.BACKEND_PORT;
+    static #getBackendHost(){
+        return process.env.BAKEND_HOST + ":" + process.env.BACKEND_PORT;        
+    }
 
-    static options = {
-        headers: {
+    static #getDefaultHeaders(){
+        return {
             'Content-Type': 'application/json',
-            'AUTHORIZATION': 
+            'AUTHORIZATION': `Basic ${Cookie.getUserToken()}`
         }
     }
 
     static async get(path) {
-        return await axios.get(backendPath + path)
+        return await axios.get(this.#getBackendHost() + path, {
+            headers: this.#getDefaultHeaders()
+        })
     }
 
     static async post(path, body) {
-        return await axios.post(backendPath + path, body)
+        return await axios.post(this.#getBackendHost() + path, body, {
+            headers: this.#getDefaultHeaders()
+        })
     }
 }
--- web-app/src/utils/cookie.js
+++ web-app/src/utils/cookie.js
@@ -1,6 +1,7 @@
 import UniversalCookie from 'universal-cookie';
 
 export default class Cookie{
+
     static cookie = new UniversalCookie();
 
     static setCookie(key, value){
@@ -11,8 +12,12 @@ export default class Cookie{
         cookie.get(key, { path: '/' });
     }
 
-    static isUserCookieSet(){
+    static isUserTokenSet(){
         return this.isKeySet('user-token');
+    }
+
+    static getUserToken(){
+        return this.getCookie('user-token');
     }
 
     static isKeySet(key){
