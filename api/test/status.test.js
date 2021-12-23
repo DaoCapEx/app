c4152f32d0b78b0d01467c43600419c3b34203f9
Abeeb Ayinla <abeebayinla.com>
2021-12-23 17:16:56 +0100

    test status with jest

--- api/test/status.test.js
+++ api/test/status.test.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import apiConfig from '../utils/api-url';
 
 /**
  * Test the GET route for home /
  */
 describe('Status API', () => {
     it('returns status to be OK', async () => {
-        const apiUrl = 'http://localhost:2015/';
-        const res = await axios(apiUrl);
+        const url = apiConfig.home;
+        const res = await axios(url);
         const data = res.data;
         expect(data).toHaveProperty('status', 'ok');
     });
