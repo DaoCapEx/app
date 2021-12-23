21af7346b741ca0322e0309a6442e4917cf7c58f
Abeeb Ayinla <abeebayinla.com>
2021-12-23 10:49:02 +0100

    change Dao API to Status API

--- api/test/status.test.js
+++ api/test/status.test.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 /**
  * Test the GET route for home /
-*/
-describe('Dao API', () => {
+ */
+describe('Status API', () => {
     it('returns status to be OK', async () => {
         const apiUrl = 'http://localhost:2015/';
         const res = await axios(apiUrl);
